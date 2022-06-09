f0976058010aa464171d202bc1d7c3404b04cf4b
Majouo <michmaj639@student.polsl.pl>
2022-06-09 17:50:19 +0200

    Wykres szkielet

--- Data/HTTPdata.cs
+++ Data/HTTPdata.cs
@@ -10,7 +10,7 @@ namespace System_ZiMZEwGD_Blazor.Data
         {
             using (var httpClient = new HttpClient())
             {
-                var adress = new Uri(device.IP+device.Port+"/"+filename);
+                var adress = new Uri("http//"+device.IP+device.Port+"/"+filename);
                 var result = httpClient.GetAsync(adress).Result;
                 string content = result.Content.ReadAsStringAsync().Result;
                 Consumption data= new Consumption(content);
