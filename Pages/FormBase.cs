b2a632102f099d712a2719b3b377b97780f405ce
patryk <patrsio475@student.polsl.pl>
2022-06-10 18:25:00 +0200

    dane z logowania i rejestracji są przenoszone do bazy danych sql

---
+++ Pages/FormBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using System_ZiMZEwGD_Blazor.Data;
+
+namespace FormBase
+{
+    public class FormBase : ComponentBase
+    {
+        public Consumption Form
+        {
+            get;
+            set;
+        }
+        protected override Task OnInitializedAsync()
+        {
+            Form = new Consumption();
+            return base.OnInitializedAsync();
+        }
+        public void SaveForm()
+        {
+
+
+        }
+    }
+}
